982e9cd436ab109e84e6f152bb58b8d28a17cc6f
Phil Schoof <phil@Phils-MacBook-Pro.local>
2020-10-10 10:55:06 -0400

    Logging response

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-require('dotenv').config()
 const axios = require('axios');
 
 const readline = require('readline').createInterface({
@@ -6,23 +5,27 @@ const readline = require('readline').createInterface({
     output: process.stdout
   });
   
-  readline.question('Access Token Please \n', token => {
+readline.question('Access Token Please \n', token => {
     if(!token) {
-       console.log('Try with a token')
-       readline.close()
-       return;
+        console.log('Try with a token')
+        readline.close()
+        return;
     }
 
     const headers = {
         'Authorization': `Bearer ${token}`
     }
 
-    axios.get(`https://api.1up.health/fhir/dstu2/Patient/1/$everything`, {headers})
-        .then(response => {
-            console.log(response.data)
+    axios.get(`https://api.1up.health/fhir/dstu2/Patient/237153/$everything`, {headers})
+        .then(({data}) => {
+            const total = data.total;
+            const entry = data.entry;
+
+            console.log(`Total: ${total}`)
+            console.log(`Entry: ${entry}`)
         })
         .catch(err => console.log(err))
 
-    
+
     readline.close();
-  });
+});
